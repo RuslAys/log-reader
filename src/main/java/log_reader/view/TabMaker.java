e27add50cdcd556e307da1c31a4a29377c75c6ed
ruslan <chippo78@mail.ru>
2018-08-05 16:51:06 +0300

    add selectors to tab

--- src/main/java/log_reader/view/TabMaker.java
+++ src/main/java/log_reader/view/TabMaker.java
@@ -2,33 +2,69 @@ package log_reader.view;
 
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import log_reader.core.FileExplorer;
 import log_reader.core.FileHolder;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class TabMaker {
 
+    private int i = 0;
+    private FileHolder holder;
+    ListView<String> listView = new ListView<>();
+
     void openTab(TabPane pane, TreeItem<FileHolder> item){
         Tab tab = new Tab();
-        FileHolder holder = item.getValue();
+        holder = item.getValue();
         File file = holder.getFile();
 
         tab.setText(file.getName());
-        ListView<String> listView = new ListView<>();
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         listView.setCellFactory(TextFieldListCell.forListView());
         listView.setOnEditCommit(event ->
             listView.getItems().set(event.getIndex(), event.getNewValue()));
-        listView.setEditable(true);
+//        listView.setEditable(true);
 
         FileExplorer fileExplorer = new FileExplorer();
         fileExplorer.addTextToListView(file, listView);
 
-        tab.setContent(listView);
+        Button down = new Button();
+        down.setText("Down");
+        down.setOnAction(event -> {
+            if(i < holder.getIndexes().size()){
+                selectItem();
+                i++;
+            }
+        });
+
+        Button up = new Button();
+        up.setText("Up");
+        up.setOnAction(event -> {
+            if(i > 0){
+                selectItem();
+                i--;
+            }
+        });
+
+        Button selectAll = new Button();
+        selectAll.setText("Select all");
+        selectAll.setOnAction(event ->
+            listView.getSelectionModel().selectAll());
+        VBox vBox = new VBox();
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(up, down, selectAll);
+        vBox.getChildren().addAll(hBox, listView);
+
+        tab.setContent(vBox);
         pane.getTabs().add(tab);
+    }
+
+    private void selectItem(){
+        int pos = holder.getIndexes().get(i).getRow();
+        listView.getSelectionModel().clearSelection();
+        listView.getSelectionModel().select(pos);
+        listView.scrollTo(pos);
     }
 }
